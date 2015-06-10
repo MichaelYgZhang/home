a80e59d4f694f9fcf3ad295b3259e7d79789ecba
MichaelYgZhang <g.zhangyg@gmail.com>
2015-06-10 09:09:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function() {
+			$("#demo-dropdown .dropdown-menu a").click(function() {
+				var href = $(this).attr("href");
+
+				$("#myTab a[href='" + href + "']").tab("show");
+
+			});
+		});
