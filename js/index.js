8a2cce7d83fa57bc800d4f99f48473e6dd5cea58
bjygzhang <bjzhangyingang@gmail.com>
2015-06-18 00:50:38 +0800

    update css and add html5 note

--- js/index.js
+++ js/index.js
@@ -1,108 +1,67 @@
-var obj = {
-	t: document.getElementById("layoutTarget"),
-	h: document.getElementById("layoutH"),
-	v: document.getElementById("layoutV")
-};
-obj.h.onclick = function(){
-	//判断当前布局
-	if(this.className === "cl on"){
-		//当前非此布局，进行切换
-		obj.t.className = "layout";
-		this.className = "cl";
-		obj.v.className = "cl on";
-	}
-	return false;
-};
-obj.v.onclick = function(){
-	//判断当前布局
-	if(this.className === "cl on"){
-		//当前非此布局，进行切换
-		obj.t.className = "layout newview";
-		this.className = "cl";
-		obj.h.className = "cl on";
-	}
-	return false;
-};
-
-$(document).ready(function() {
-	console.log("页面加载完成。。。");
-});
-
+//window.onbeforeunload = function(event) {
+//	(event || window.event).returnValue = "感谢来到小站,期待下次交流,谢谢！";
+//}
 var app = angular.module('app',['ngAnimate']);
-/**
-app.directive('myTabs', function() {
-		return {
-			restrict: 'E',
-			transclude: true,
-			scope: {},
-			controller: function($scope) {
-				var panes = $scope.panes = [];
 
-				$scope.select = function(pane) {
-					angular.forEach(panes, function(pane) {
-						pane.selected = false;
-					});
-					pane.selected = true;
-				};
-
-				this.addPane = function(pane) {
-					if (panes.length === 0) {
-						$scope.select(pane);
-					}
-					panes.push(pane);
-				};
-			},
-			templateUrl: 'common/my-tabs.html'
-		};
-	})
-	.directive('myPane', function() {
-		return {
-			require: '^myTabs',
-			restrict: 'E',
-			transclude: true,
-			scope: {
-				title: '@'
-			},
-			link: function(scope, element, attrs, tabsCtrl) {
-				tabsCtrl.addPane(scope);
-			},
-			templateUrl: 'common/my-pane.html'
-		};
-	});
-
-
-
-
- <li class="dropdown">
- <a href="#" class="dropdown-toggle" data-toggle="dropdown">
- JAVA
- <span class="caret"></span></a>
- <ul class="dropdown-menu" role="menu">
- <li><a href="#tab-chrome">Java SE</a></li>
- <li><a href="#tab-firefox">Java EE</a></li>
- <li><a href="#tab-ie">Java WEB</a></li>
- <li><a href="#tab-ie">Java API</a></li>
- <li><a href="#tab-ie">Tools</a></li>
- </ul>
- </li>
-*/
 app.controller('indexController', ['$scope', function($scope) {
-		$scope.web =
-				[ { name: 'Flex-ActionScript', url: 'web/flex-as/flex_as_index.html'},
-				{ name: 'CSS3', url: 'web/css/css3_index.html'},
-				{ name: 'HTML5', url: 'web/html5/h5_index.html'},
-				{ name: 'Bootstrap', url: 'web/bootstrap/bootstrap_index.html'},
-				{ name: 'AngularJS', url: 'web/angularjs/angularjs_index.html'},
-				];
-		$scope.java =
-				[ { name: 'Java SE', url: 'java/java-se/javase_index.html'},
-				{ name: 'Java EE', url: 'java/java-ee/javaee_index.html'},
-				{ name: 'Java WEB', url: 'java/java-web/javaweb_index.html'},
-				{ name: 'Java API', url: 'java/java-api/javaapi_index.html'},
-				];
-		//$scope.template = $scope.web[0];
+
+	$scope.blogs = [
+					{ahref:'WEB/HTML5/read-html5-cookbook.html',imgsrc:'http://www.w3.org/html/logo/img/mark-word-icon.png',titletext:'HTML5',spantext:'Reading HTML5 Cookbook Note.'}
+				   ];
+
+	$scope.islayout = false;
+	$scope.isnewview = true;
+	$scope.islayout.className = "c1 on";
+
+	$scope.showV = function(){
+		$scope.islayout = false;
+		$scope.isnewview = true;
+		$scope.islayout.className = "c1 on";
+	};
+	$scope.showH = function(){
+		$scope.islayout = true;
+		$scope.isnewview = false;
+		$scope.islayout.className = "c1";
+	};
+
+
+	$scope.showContent = function(blog){
+		$scope.isShowContent = true;
+		$scope.template = {url:""+blog.ahref+""};
+	};
+
 }]);
 
+/**
+ * dom方式 操作横向布局还是列布局
+ */
+
+//var obj = {
+//	t: document.getElementById("layoutTarget"),
+//	h: document.getElementById("layoutH"),
+//	v: document.getElementById("layoutV")
+//};
+//
+//obj.h.onclick = function(){
+//	//判断当前布局
+//	if(this.className === "cl on"){
+//		//当前非此布局，进行切换
+//		obj.t.className = "layout";
+//		this.className = "cl";
+//		obj.v.className = "cl on";
+//	}
+//	return false;
+//};
+//obj.v.onclick = function(){
+//	//判断当前布局
+//	if(this.className === "cl on"){
+//		//当前非此布局，进行切换
+//		obj.t.className = "layout newview";
+//		this.className = "cl";
+//		obj.h.className = "cl on";
+//	}
+//	return false;
+//};
 
 /**
  * backToTop
