6d06fb40a12784751e438d0a88b1c7072811944d
MichaelYgZhang <g.zhangyg@gmail.com>
2015-07-15 20:08:25 +0800

    add blog
    add  angularjs controller communication blog.

--- js/index.js
+++ js/index.js
@@ -43,6 +43,29 @@ app.controller('indexController', function($scope,$http) {
 
 
 });
+app.controller("parentCtr",
+	function ($scope) {
+		$scope.$on("Ctr1NameChange",
+
+			function (event, msg) {
+				console.log("parent", msg);
+				$scope.$broadcast("Ctr1NameChangeFromParrent", msg);
+			});
+	}).controller("childCtr1", function ($scope) {
+		$scope.change = function (name) {
+			console.log("childCtr1", name);
+			$scope.$emit("Ctr1NameChange", name);
+		};
+	}).controller("childCtr2", function ($scope) {
+		$scope.$on("Ctr1NameChangeFromParrent",
+
+			function (event, msg) {
+				console.log("childCtr2", msg);
+				$scope.ctr1Name = msg;
+			});
+	});
+
+
 
 /**
  * dom方式 操作横向布局还是列布局
