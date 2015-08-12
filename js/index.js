1c836eab8e6520b80ad11798751024618364949e
bjygzhang <bjzhangyingang@gmail.com>
2015-08-12 23:07:21 +0800

    update code to jsfiddle.

--- js/index.js
+++ js/index.js
@@ -41,27 +41,6 @@ app.controller('indexController', function($scope,$http) {
 
 
 });
-app.controller("parentCtr",
-	function ($scope) {
-		$scope.$on("Ctr1NameChange",
-
-			function (event, msg) {
-				console.log("parent", msg);
-				$scope.$broadcast("Ctr1NameChangeFromParrent", msg);
-			});
-	}).controller("childCtr1", function ($scope) {
-		$scope.change = function (name) {
-			console.log("childCtr1", name);
-			$scope.$emit("Ctr1NameChange", name);
-		};
-	}).controller("childCtr2", function ($scope) {
-		$scope.$on("Ctr1NameChangeFromParrent",
-
-			function (event, msg) {
-				console.log("childCtr2", msg);
-				$scope.ctr1Name = msg;
-			});
-	});
 
 
 	app.directive('myDirective',function(){
